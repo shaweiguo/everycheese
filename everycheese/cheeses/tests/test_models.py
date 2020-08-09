b2092db059db04da3571535bd2858d7ee047b8d8
shaweiguo <shaweiguo@ymail.com>
2020-08-09 12:01:52 +0800

    Use CheeseFactory in a test

--- everycheese/cheeses/tests/test_models.py
+++ everycheese/cheeses/tests/test_models.py
@@ -1,16 +1,21 @@
 import pytest
 
 from ..models import Cheese
+from .factories import CheeseFactory
 
 
 pytestmark = pytest.mark.django_db
 
 
 def test___str__():
-    cheese = Cheese.objects.create(
-        name='Stracchino',
-        description='Semi-sweet cheese that goes well with starches.',
-        firmness=Cheese.Firmness.SOFT,
-    )
-    assert cheese.__str__() == 'Stracchino'
-    assert str(cheese) == 'Stracchino'
+    # cheese = Cheese.objects.create(
+    #     name='Stracchino',
+    #     description='Semi-sweet cheese that goes well with starches.',
+    #     firmness=Cheese.Firmness.SOFT,
+    # )
+    # cheese = CheeseFactory(name='Stracchino')
+    # assert cheese.__str__() == 'Stracchino'
+    # assert str(cheese) == 'Stracchino'
+    cheese = CheeseFactory()
+    assert cheese.__str__() == cheese.name
+    assert str(cheese) == cheese.name
--- everycheese/cheeses/urls.py
+++ everycheese/cheeses/urls.py
@@ -10,4 +10,9 @@ urlpatterns = [
         view=views.CheeseListView.as_view(),
         name='list'
     ),
+    path(
+        route='<slug:slug>/',
+        view=views.CheeseDetailView.as_view(),
+        name='detail'
+    ),
 ]
